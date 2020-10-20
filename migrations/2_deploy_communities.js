c6450e225b158f6eb60818408d87b5a64e69152b
Lorenzo Bersano <lbersano@themis.it>
2020-10-20 16:18:13 +0200

    Made migrations in order to deploy 3 test communities

--- migrations/2_deploy_communities.js
+++ migrations/2_deploy_communities.js
@@ -1,7 +1,9 @@
 const Community = artifacts.require("Community");
 
+const GSN_ROPSTEN_FORWARDER = "0x25CEd1955423BA34332Ec1B60154967750a0297D";
+
 module.exports = function (deployer) {
-  deployer.deploy(Community);
-  deployer.deploy(Community);
-  deployer.deploy(Community);
+  deployer.deploy(Community, GSN_ROPSTEN_FORWARDER);
+  deployer.deploy(Community, GSN_ROPSTEN_FORWARDER);
+  deployer.deploy(Community, GSN_ROPSTEN_FORWARDER);
 };
