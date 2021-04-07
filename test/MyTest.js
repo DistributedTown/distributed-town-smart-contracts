e0f6bbd5591604b2787579a838e1731933bcfdf4
migrenaa <mimonova13@gmail.com>
2021-04-07 15:53:20 +0300

    added tests covering create community

--- test/MyTest.js
+++ test/MyTest.js
@@ -10,9 +10,28 @@ describe("DistributedTown", function () {
   describe("CommunityRegistry", function () {
     it("Should deploy CommunityRegistry", async function () {
       const CommunityRegistryContract = await ethers.getContractFactory("CommunitiesRegistry");
-
       communityRegistryContract = await CommunityRegistryContract.deploy('0x89AB41fceb97324BEf2Aa5d5048F13b71b8fbca2');
     });
+
+    describe("createCommunity()", async function () {
+      it("Should create a genesis community", async function () {
+        const community = await communityRegistryContract.createCommunity(
+            "https://hub.textile.io/thread/bafkwfcy3l745x57c7vy3z2ss6ndokatjllz5iftciq4kpr4ez2pqg3i/buckets/bafzbeiaorr5jomvdpeqnqwfbmn72kdu7vgigxvseenjgwshoij22vopice",
+            0,
+            0,
+            'GenesisTown',
+            3,
+            6,
+            12,
+            24
+        );
+        const communities = await communityRegistryContract.getCommunities();
+        const numOfComs = await communityRegistryContract.numOfCommunities();
+        expect(numOfComs).to.eq(1);
+        expect(communities).to.be.an('array');
+        expect(communities.length).to.eq(1);
+      });
+    })
 
     describe("getCommunities()", function () {
       it("Should be able to get the communities", async function () {
