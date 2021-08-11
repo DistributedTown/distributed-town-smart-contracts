eb0980861124b9dcbddb3ab52d1d7b8bb600ee4c
Milena Monova <mimonova13@gmail.com>
2021-08-11 21:16:41 +0300

    added getMemberAddresses to ICommunity

--- contracts/community/ICommunity.sol
+++ contracts/community/ICommunity.sol
@@ -34,6 +34,7 @@ interface ICommunity {
     function leave(address memberAddress) external;
 
     function getMembers() external view returns (uint256[] memory);
+    function getMemberAddresses() public view override returns (address[] memory);
 
     // TODO: check called only by milestones!
     function transferToCommunity(address from, uint256 amount) external;
