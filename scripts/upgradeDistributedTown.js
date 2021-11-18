effaf709f85a55ecbf4cacc05bf5041174c13f09
evgeni <evgeni@leapdao.org>
2021-11-18 16:44:37 +0100

    add new dito address

--- scripts/upgradeDistributedTown.js
+++ scripts/upgradeDistributedTown.js
@@ -2,7 +2,8 @@ const { ethers, upgrades } = require('hardhat');
 
 async function main () {
 
-  const distributedTownProxyAddress = '0x2d5F9858a1656163327908D623cfe1255fd589Fa';
+  //const distributedTownProxyAddress = '0x2d5F9858a1656163327908D623cfe1255fd589Fa';
+  const distributedTownProxyAddress = '0x351A579a3b5f35B874b20ff710A555252D25d1c9';
 
 
   const DistributedTown = await ethers.getContractFactory('DistributedTown');
